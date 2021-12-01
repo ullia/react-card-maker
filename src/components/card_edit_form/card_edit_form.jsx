f5cfac23b62f3ad8cfd0e8a6f110ff18cb80125a
Ullia <antales21@naver.com>
2021-12-01 14:30:16 +0900

    add card edit form

--- src/components/card_edit_form/card_edit_form.jsx
+++ src/components/card_edit_form/card_edit_form.jsx
@@ -1,4 +1,6 @@
 import React, { useRef, useState } from 'react';
+import Button from '../button/button';
+import ImageFileInput from '../image_file_input/image_file_input';
 import styles from './card_edit_form.module.css';
 
 const CardEditForm = ({ card }) => {
@@ -8,19 +10,24 @@ const CardEditForm = ({ card }) => {
   const onTp = (event) => {
     const txt = event.currentTarget.value;
     console.log(txt);
+    const onSubmit = () => {};
   };
   return (
     <form className={styles.form}>
       <input ref={nameRef} name='name' onKeyPress={onTp} type='text' value={name} className={styles.input} />
-      <input type='text' className={styles.input} />
-      <select type='select' className={styles.select}>
-        <option value='1'>1</option>
+      <input name='company' type='text' value={company} className={styles.input} />
+      <select name='theme' type='select' className={styles.select}>
+        <option value='Light'>Light</option>
+        <option value='Dark'>Dark</option>
+        <option value='Coloful'>Coloful</option>
       </select>
-      <input type='text' className={styles.input} />
-      <input type='text' className={styles.input} />
-      <textarea className={styles.textarea} name='' id='' cols='30' rows='10'></textarea>
-      <div className={styles.fileInput}></div>
-      <button name='edit'></button>
+      <input name='title' type='text' value={title} className={styles.input} />
+      <input name='email' type='text' value={email} className={styles.input} />
+      <textarea className={styles.textarea} name='message' id='' cols='30' rows='10'></textarea>
+      <div className={styles.fileInput}>
+        <ImageFileInput />
+        <Button name='Delete' onClick={onsubmit} />
+      </div>
     </form>
   );
 };
